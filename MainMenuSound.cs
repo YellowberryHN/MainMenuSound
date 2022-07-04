84fa15105d63e0c5e0ca4d738d288cd5f4df3266
Zsolt Zitting <yellowberry@cocaine.ninja>
2022-07-04 01:28:28 -0600

    Add project files.

---
+++ MainMenuSound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MainMenuSound
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class MainMenuSound : MonoBehaviour
+	{
+		public string StartSound = "MainMenuSound/Start";
+
+		private void Start()
+		{
+			print("MainMenuSound: Game has started!");
+
+			if (!GameDatabase.Instance.ExistsAudioClip(StartSound))
+			{
+				Debug.LogError("MainMenuSound: Audio file not found: " + StartSound);
+				return;
+			}
+
+			var audio = gameObject.AddComponent<AudioSource>();
+			audio.clip = GameDatabase.Instance.GetAudioClip(StartSound);
+			audio.Play();
+			audio.loop = false;
+		}
+	}
+}
